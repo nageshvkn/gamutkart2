5e8d31896b4987291fbd69b9901a8cf680ad7664
P Nageswara Rao <nagesh@slfjslfj.com>
2018-02-21 20:19:00 +0530

    modified workspace path to generic

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 if [ $ENVIRONMENT = "QA" ]; then
 #
 # This deploys the war file into tomcat's webapps directory
-  sshpass -p "gamut" scp /home/gamut/.jenkins/workspace/flipkart_build/target/gamutkart.war gamut@172.17.0.2:/home/gamut/apache-tomcat-8.5.23/webapps
+  sshpass -p "gamut" scp $WORKSPACE/target/gamutkart.war gamut@172.17.0.2:/home/gamut/apache-tomcat-8.5.23/webapps
 #
 # This login's to tomcat machine, sets java_home environment variable, starts tomcat server
 # and changes permission for tomcat folder to 777
@@ -12,7 +12,7 @@ if [ $ENVIRONMENT = "QA" ]; then
 
 elif [ $ENVIRONMENT = "SIT" ]; then
   echo deploying the code into SIT environment...
-  sshpass -p "gamut" scp /home/gamut/.jenkins/workspace/flipkart_build/target/gamutkart.war gamut@172.17.0.3:/home/gamut/apache-tomcat-8.5.23/webapps
+  sshpass -p "gamut" scp $WORKSPACE/target/gamutkart.war gamut@172.17.0.3:/home/gamut/apache-tomcat-8.5.23/webapps
   #
   sshpass -p "gamut" ssh gamut@172.17.0.3 "JAVA_HOME=/home/gamut/jdk1.8.0_151" "/home/gamut/apache-tomcat-8.5.23/bin/startup.sh" "chmod 777 -R /home/gamut/apache-tomcat-8.5.23"
 fi
