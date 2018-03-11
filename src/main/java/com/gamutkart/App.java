65006292860d6cb7b54f1dfe2e717a17865e2c5c
Harry <Harry@harry.com>
2018-03-11 15:27:40 +0530

    iterate 100 times

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=10;i++)
+		for(i=0;i<=100;i++)
 		{
         	System.out.println( "print number: " + i);
    		}
