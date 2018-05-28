3a49ec9c6d37572a69cf88beb77f868f017e4c11
P Nageswara Rao <nageswara.rao@gamutgurus.com>
2018-05-28 08:46:12 +0530

    iterate only 200 times

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=1000;i++)
+		for(i=0;i<=200;i++)
 		{
 			i += 3;
         	System.out.println( "print number: " + i);
