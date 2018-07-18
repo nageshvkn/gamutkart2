5946d02d269895b362b6e92e3c02481b14c09d80
P Nageswara Rao <nageswara.rao@gamutgurus.com>
2018-07-18 10:36:33 +0530

    iterate 200 times. cleaned up some code

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,10 +5,9 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=100;i++)
+		for(i=0;i<=200;i++)
 		{
 			i += 4;
-        	System.out.println( "print number: " + i);
         	System.out.println( "print number: " + i);
    		}
 	 }
