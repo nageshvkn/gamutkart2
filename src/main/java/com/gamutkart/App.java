a0caa4f1cdcf090a5d0436772557c704a8fd4abd
P Nageswara Rao <nageswara.rao@gamutgurus.com>
2018-09-17 09:52:59 +0530

    ilsjflsfj

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=100;i++)
+		for(i=0;i<=500;i++)
 		{
 			i += 2;
         	System.out.println( "print number: " + i);
