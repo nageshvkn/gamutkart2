758a652f8aaf77857a52bcb3e81ca2750c39bffa
P Nageswara Rao <blabla@blabla.com>
2018-09-23 11:17:22 +0530

    sfsf

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ENVIRONMENT=$1
 if [ $ENVIRONMENT = "QA" ];then
 	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.2:/home/gamut/Distros/apache-tomcat-8.5.23/webapps
 	sshpass -p "gamut" ssh gamut@172.17.0.2 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_151" "/home/gamut/Distros/apache-tomcat-8.5.23/bin/startup.sh"
