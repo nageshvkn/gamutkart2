e48228866265902632f59bdcc69839947b02ffd6
P Nageswara Rao <blabla@blabla.com>
2018-10-15 09:26:10 +0530

    modified the code to print numbers skipping by 3

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,10 +5,9 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=500;i++)
+		for(i=0;i<=200;i++)
 		{
-			i += 2;
-        	System.out.println( "print number: " + i);
+			i += 3;
         	System.out.println( "print number: " + i);
    		}
 	 }
