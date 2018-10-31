c422beec405ab2f0fb04ca5934862cd680f83006
P Nageswara Rao <blabla@bla.com>
2018-10-31 16:46:09 +0530

    deployment status info added

--- deploy.sh
+++ deploy.sh
@@ -9,5 +9,6 @@ if [ $ENVIRONMENT = "QA" ];then
 elif  [ $ENVIRONMENT = "SIT" ];then
 	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.3:/home/gamut/Distros/apache-tomcat-8.5.23/webapps
 	sshpass -p "gamut" ssh gamut@172.17.0.3 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_151" "/home/gamut/Distros/apache-tomcat-8.5.23/bin/startup.sh"
+echo "deployment has been done!"
 fi
 
