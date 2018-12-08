e340ef5ee82a3d591f7fa17592110920ec614f58
P Nageswara Rao <nageswara.rao@gamutgurus.com>
2018-12-08 17:31:11 +0530

    change to test poll scm jenkins option

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=300;i++)
+		for(i=0;i<=400;i++)
 		{
 			i += 3;
         	System.out.println( "print number: " + i);
