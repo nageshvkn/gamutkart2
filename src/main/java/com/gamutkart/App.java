e1f7ea3cd137796befa7d07366246139c9f335b3
P Nageswara Rao <nageswara.rao@blabla.com>
2019-02-17 11:07:54 +0530

    modified code for fixbug123.

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,10 +5,9 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=400;i++)
+		for(i=0;i<=100;i++)
 		{
 			i += 3;
-        	System.out.println( "print number: " + i);
         	System.out.println( "print number: " + i);
    		}
 	 }
