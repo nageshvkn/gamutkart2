aa2f9a368f0d29e04bede1f1c0c1d99e1550a726
P Nageswara Rao <nageswara.rao@nexamatic.com>
2019-03-20 09:30:07 +0530

    iterate 200 times

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,7 +5,7 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-		for(i=0;i<=100;i++)
+		for(i=0;i<=200;i++)
 		{
 			i += 3;
         	System.out.println( "print number: " + i);
