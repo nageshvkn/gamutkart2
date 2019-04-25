3b14f2e2f6c2fe8ec5eaa46f8b7cff78914831bd
Your Name <you@example.com>
2019-04-25 11:50:34 +0530

    changed tomcat path

--- multi_deploy.sh
+++ multi_deploy.sh
@@ -8,7 +8,7 @@ for i in `cat multi-deploy-IPs.txt`
 	sshpass -p "gamut" scp target/gamutkart.war gamut@$i:/home/gamut/Distros/apache-tomcat-8.5.38/webapps
 #
 	echo "Starting tomcat server in $i QA server.."
-	sshpass -p "gamut" ssh gamut@$i "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_151" "/home/gamut/Distros/apache-tomcat-8.5.38/webapps/bin/startup.sh"
+	sshpass -p "gamut" ssh gamut@$i "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_151" "/home/gamut/Distros/apache-tomcat-8.5.38/bin/startup.sh"
 #
 	done
 	echo "Deployment is succussful!"
