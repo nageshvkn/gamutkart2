5f1da44e260cba937a4be79a4e16f09883f40a61
P Nageswara Rao <info@gamutgurus.com>
2019-05-26 12:08:53 +0530

    iterate 40 times

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,10 +5,10 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-// testing continuous buids in Jenkins
-		for(i=0;i<=40;i++)
+		for(i=0;i<=70;i++)
 		{
 			i += 3;
+        	System.out.println( "print number: " + i);
         	System.out.println( "print number: " + i);
    		}
 	 }
