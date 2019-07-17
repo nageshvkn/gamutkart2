d9ebe84028e8dcede30e02e621c4e49fe73f9310
P Nageswara Rao <nageswara.rao@gamutgurus.com>
2019-07-17 10:02:23 +0530

    update script

--- create-env.sh
+++ create-env.sh
@@ -7,8 +7,9 @@ do
 	echo "=============================="
     echo "Creating www.gamutkart$i container.."
     sleep 1
-    docker run --name www.gamutkart$i -d -it --rm nageshvkn/gamutkart-img /bin/bash
+    docker run --name www.gamutkart$i -d -it nageshvkn/gamutkart-img /bin/bash
     echo "www.gamutkart$i container has been created!"
 	echo "=============================="
 done
-docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -a -q` > IPs.txt
+docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > IPs.txt
+#./create_lb.sh IPs.txt
