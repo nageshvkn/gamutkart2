296d37d46d9b1cafc2009c65550e254159899223
Nageswara Rao P <info@gamutgurus.com>
2020-03-22 16:12:34 +0530

    itereate 700 times

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,8 +5,8 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-// iterate for 600 times
-		for(i=0;i<=600;i++)
+// iterate for 700 times
+		for(i=0;i<=700;i++)
 		{
 			i += 4;
    		}
