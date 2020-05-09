96411da2f77be56aa3568e0309c4fb0fa90cf253
Nageswara Rao P <info@gamutgurus.com>
2020-05-09 12:55:02 +0530

    iterate 800 times

--- src/main/java/com/gamutkart/App.java
+++ src/main/java/com/gamutkart/App.java
@@ -5,8 +5,8 @@ public class App
     public static void main( String[] args )
     {
 		int i;
-// iterate for 700 times
-		for(i=0;i<=700;i++)
+// iterate for 800 times
+		for(i=0;i<=800;i++)
 		{
 			i += 4;
    		}
