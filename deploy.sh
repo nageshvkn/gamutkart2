d371c042698619c0d8b6d2fec839ccc708928427
P Nageswara Rao <info@wicutly.com>
2020-06-23 07:27:59 +0530

    install path changed

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,15 @@
 ##
 ENVIRONMENT=$1
 if [ $ENVIRONMENT = "QA" ];then
-	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.3:/home/gamut/Distros/apache-tomcat-8.5.38/webapps
-	sshpass -p "gamut" ssh gamut@172.17.0.3 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_151" "/home/gamut/Distros/apache-tomcat-8.5.38/bin/startup.sh"
 
-elif  [ $ENVIRONMENT = "SIT" ];then
-	sshpass -p "gamut" scp target/gamutkart.war gamut@172.17.0.4:/home/gamut/Distros/apache-tomcat-8.5.38/webapps
-	sshpass -p "gamut" ssh gamut@172.17.0.4 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_151" "/home/gamut/Distros/apache-tomcat-8.5.38/bin/startup.sh"
-echo "deployment has been done!"
+    sshpass -p "gamut" scp target/gamutgurus.war gamut@172.17.0.3:/home/gamut/Distros/apache-tomcat-8.5.50/webapps
+
+    sshpass -p "gamut" ssh gamut@172.17.0.3 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_241" "/home/gamut/Distros/apache-tomcat-8.5.50/bin/startup.sh"
+
+elif [ $ENVIRONMENT = "SIT" ];then
+    sshpass -p "gamut" scp target/gamutgurus.war gamut@172.17.0.4:/home/gamut/Distros/apache-tomcat-8.5.50/webapps
+
+    sshpass -p "gamut" ssh gamut@172.17.0.4 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_241" "home/gamut/Distros/apache-tomcat-8.5.50/bin/startup.sh"
+
 fi
 
