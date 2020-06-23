b2fc225d76fb945ceed0625ef52a3da37e5a4c1b
P Nageswara Rao <info@wicutly.com>
2020-06-23 07:32:18 +0530

    install path changed

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ if [ $ENVIRONMENT = "QA" ];then
 elif [ $ENVIRONMENT = "SIT" ];then
     sshpass -p "gamut" scp target/gamutgurus.war gamut@172.17.0.4:/home/gamut/Distros/apache-tomcat-8.5.50/webapps
 
-    sshpass -p "gamut" ssh gamut@172.17.0.4 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_241" "home/gamut/Distros/apache-tomcat-8.5.50/bin/startup.sh"
+    sshpass -p "gamut" ssh gamut@172.17.0.4 "JAVA_HOME=/home/gamut/Distros/jdk1.8.0_241" "/home/gamut/Distros/apache-tomcat-8.5.50/bin/startup.sh"
 
 fi
 
