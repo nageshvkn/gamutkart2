bfcbdc645d0a49961c4523162ca16377ae61adf5
Nageswara Rao P <info@wiculty.com>
2021-05-02 21:54:48 +0530

    update script

--- create-env.sh
+++ create-env.sh
@@ -5,10 +5,10 @@ sleep 2;
 for i in `seq $cont_count`
 do
 	echo "=============================="
-    echo "Creating www.gamutkart$i container.."
+    echo "Creating tomcat-server$i container.."
     sleep 1
-    docker run --name www.gamutkart$i -d -it nageshvkn/gamutkart-img /bin/bash
-    echo "www.gamutkart$i container has been created!"
+    docker run -it -d --name www.gamutkart$i nageshvkn/gamutkart-img
+    echo "tomcat-server$i container has been created!"
 	echo "=============================="
 done
 docker inspect --format {{.NetworkSettings.Networks.bridge.IPAddress}} `docker ps -q` > IPs.txt
